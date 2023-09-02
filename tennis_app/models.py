76a7e82de4569263a7e9e46916db0c853f8b7a74
frank@gmail.com <imafidonfrank2015@gmail.com>
2023-09-02 16:03:28 +0100

    first commit

---
+++ tennis_app/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Signup (models.Model):
+    fname = models.CharField(max_length=255)
+    lname = models.CharField(max_length=255)
+    dob =   models.DateField()
+    email = models.CharField(max_length=255)
+    password = models.CharField(max_length=255, default="")
+
+
+    def __str__(self):
+        return self.fname
+
+
---
+++ tennis_app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Signup
+from .serializers import SignupSerializer
+
+class SignupView(viewsets.ModelViewSet):
+    queryset = Signup.objects.all()
+    serializer_class = SignupSerializer
