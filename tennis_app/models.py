96cc353a7dd1b94710a2d1b119d0d60518cfaa03
frank@gmail.com <imafidonfrank2015@gmail.com>
2023-09-06 16:43:19 +0100

    first commit

--- tennis_app/models.py
+++ tennis_app/models.py
@@ -9,10 +9,16 @@ class Signup (models.Model):
     nationality = models.CharField(max_length=255, default="")
     handbat = models.CharField(max_length=255, default="")
 
+    def __str__(self):
+        return self.fname
+
 class PlayerRanking (models.Model):
     first_name = models.ForeignKey(Signup, on_delete=models.CASCADE, to_field="fname", related_name='first_name')
     last_name = models.ForeignKey(Signup, on_delete=models.CASCADE, to_field="lname", related_name='last_name')
     points = models.IntegerField()
+
+    def __str__(self):
+        return self.first_name
 
 class Profile (models.Model):
     profile= models.CharField(max_length=255)
