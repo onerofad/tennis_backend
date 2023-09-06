03fbaf7e907384da014858d4175ee6b17d1fb010
frank@gmail.com <imafidonfrank2015@gmail.com>
2023-09-06 21:29:15 +0100

    first commit

--- tennis_app/serializers.py
+++ tennis_app/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
-from .models import Signup
+from .models import Signup, PlayerRanking
 
 class SignupSerializer(serializers.ModelSerializer):
     class Meta:
         model = Signup
         fields = ('id', 'fname', 'lname', 'dob', 'email', 'password', 'nationality', 'handbat')
+
+class PlayerRankingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PlayerRanking
+        fields = ('id', 'firstname', 'points')
 
--- tennis_app/views.py
+++ tennis_app/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Signup
-from .serializers import SignupSerializer
+from .serializers import SignupSerializer, PlayerRankingSerializer
 
 class SignupView(viewsets.ModelViewSet):
     queryset = Signup.objects.all()
     serializer_class = SignupSerializer
+
+class PlayerRankingView(viewsets.ModelViewSet):
+    queryset = Signup.objects.all()
+    serializer_class = PlayerRankingSerializer
 
 
